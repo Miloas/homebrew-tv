0cec45b7b023fad604acb2d2a89b553f9bb9aa6a
miloas <0x9357@gmail.com>
2019-08-27 18:22:28 +0800

    chore: update readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # homebrew-tv
+
+Homebrew repository to install the tv command line utility
+
+# install
+
+```sh
+brew tap Miloas/tv
+brew install tv
+```
--- tv.rb
+++ tv.rb
@@ -3,6 +3,7 @@ class Tv < Formula
   homepage "https://github.com/Miloas/tv"
   url "https://github.com/Miloas/tv/releases/download/1.0.4/tv_darwin_amd64"
   version "1.0.4"
+  sha256 "cdbbbdc4d2dfc7c197677e85540c9c37c0fc98c2c4458cedee5cfbaf07447b5c"
 
   def install
     nm = "tv"
