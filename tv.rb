8371e2d13cc9c7ab5dbad771d760c6ce0f3b70b8
miloas <0x9357@gmail.com>
2019-08-29 03:11:46 +0800

    bump version

--- tv.rb
+++ tv.rb
@@ -1,9 +1,8 @@
 class Tv < Formula
   desc "Tag version cli"
   homepage "https://github.com/Miloas/tv"
-  url "https://github.com/Miloas/tv/releases/download/1.0.4/tv_darwin_amd64"
-  version "1.0.4"
-  sha256 "cdbbbdc4d2dfc7c197677e85540c9c37c0fc98c2c4458cedee5cfbaf07447b5c"
+  url "https://github.com/Miloas/tv/releases/download/1.0.7/tv_darwin_amd64"
+  version "1.0.7"
 
   def install
     nm = "tv"
