13ec9c565ccacdcf1be535ba1a1f8aaa0fc765b6
Miloas <0x9357@gmail.com>
2019-09-06 19:30:32 +0800

    Update tv.rb

--- tv.rb
+++ tv.rb
@@ -1,8 +1,8 @@
 class Tv < Formula
   desc "Tag version cli"
   homepage "https://github.com/Miloas/tv"
-  url "https://github.com/Miloas/tv/releases/download/1.0.9/tv_darwin_amd64"
-  version "1.0.9"
+  url "https://github.com/Miloas/tv/releases/download/1.0.10/tv_darwin_amd64"
+  version "1.0.10"
 
   def install
     nm = "tv"
